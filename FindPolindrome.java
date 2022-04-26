bc9032ab874e6799323c674e7689d01238afac8e
sheeladevitn <92189813+sheeladevitn@users.noreply.github.com>
2022-04-26 13:18:37 -0700

    Add files via upload

---
+++ FindPolindrome.java
@@ -0,0 +1,26 @@
+package JavaCodingInterviewPackage;
+
+// This program is Polindrome
+public class FindPolindrome 
+{
+  public static void findPolindrome(String str)
+  {
+	  
+	  StringBuffer buffer = new StringBuffer(str);
+	  buffer.reverse();
+	  String s = buffer.toString();
+	  System.out.println(s);
+	  if (str.equals(s))
+	  {
+		  System.out.println("String is polindrome");
+	  }
+	  else
+	  {
+		  System.out.println("String is not polindrome");
+	  }
+  }
+	public static void main(String[] args) {
+		findPolindrome("kayak");
+	}
+
+}
