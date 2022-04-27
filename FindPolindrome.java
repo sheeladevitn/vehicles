c91e0415a6bc719467ad2b0cb7864b4176059d42
sheeladevitn <92189813+sheeladevitn@users.noreply.github.com>
2022-04-26 19:13:51 -0700

    Update FindPolindrome.java

--- FindPolindrome.java
+++ FindPolindrome.java
@@ -1,28 +1,19 @@
-package JavaCodingInterviewPackage;
+
 
 // This program is Polindrome
 public class FindPolindrome 
 {
-  public static void findPolindrome(String str)
-  {
-	  
-	  StringBuffer buffer = new StringBuffer(str);
-	  buffer.reverse();
-	  String s = buffer.toString();
-	  System.out.println(s);
-	  if (str.equals(s))
-	  {
-		  System.out.println("String is polindrome");
+  
+		  
+	 
+  
+	public static void main(String[] args)
+	{
+		System.out.println("String is polindrome");
 		  System.out.println("String is polindrome good");
-	  }
-	  else
-	  {
+	  
 		  System.out.println("String is not polindrome");
 		  System.out.println("String is polindrome good");
-	  }
-  }
-	public static void main(String[] args) {
-		findPolindrome("kayak");
 	}
 
 }
